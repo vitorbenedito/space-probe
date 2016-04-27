b517aac19c76f79020f41d71b544e3dc65716d3d
Vitor Nunes Benedito <vitorbenedito@Vitors-MacBook-Pro.local>
2016-04-27 14:05:20 -0300

    API Test

---
+++ src/test/java/com/spaceprobe/SpaceProbeApiTest.java
@@ -0,0 +1,28 @@
+package com.spaceprobe;
+
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import junit.framework.TestCase;
+
+public class SpaceProbeApiTest extends TestCase {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private final String url = "http://localhost:8080/spaceprobe";
+
+    @Test
+    public void testCreateContactWithCustomer() throws Exception {
+        restTemplate.postForLocation(url, request());
+       
+        String response = restTemplate.getForObject(url, String.class);
+        System.out.println(response);
+        assertNotNull(response);
+
+    }
+
+    private String request() {
+        return "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM";
+    }
+
+}
